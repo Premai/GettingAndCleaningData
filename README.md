da74e87a9312ca8ffe8b3b06904d0824df7bd00a
Premai <hermoine09@gmail.com>
2014-06-22 14:23:31 -0400

    Commit modifications

--- README.md
+++ README.md
@@ -3,9 +3,14 @@ GettingAndCleaningData
 
 Getting and Cleaning Data Project Assignment Repo
 
-The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. You will be graded by your peers on a series of yes/no questions related to the project. You will be required to submit: 1) a tidy data set as described below, 2) a link to a Github repository with your script for performing the analysis, and 3) a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.
+##Goal: 
+Demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. 
+1) a tidy data set 
+2) a link to a Github repository with run_analysis.R script for performing the analysis
+3) a code book that describes the variables, the data, and any transformations called CodeBook.md. 4) README.md that explains how the script work.
 
-One of the most exciting areas in all of data science right now is wearable computing - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained:
+Data collected from the accelerometers from the Samsung Galaxy S smartphone. 
+A full description is available at the site where the data was obtained:
 
 http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
@@ -13,28 +18,44 @@ Here are the data for the project:
 
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
-You should create one R script called run_analysis.R that does the following.
-
 Merges the training and the test sets to create one data set.
 Extracts only the measurements on the mean and standard deviation for each measurement.
 Uses descriptive activity names to name the activities in the data set.
 Appropriately labels the data set with descriptive activity names.
 Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-## Steps already done
-In the run_analysis.R have commented out the steps of downloading the zip file and unzipping
-as I have uploaded the directory to the GitHub repo.
+## Packages used
+reshape2
+knickr
+
+## Files in this repo
+README.md -- this is the readme
+CodeBook.md -- codebook describing variables, the data and transformations
+run_analysis.R --  R code to create tidy data
 
 ## Things to consider before running script locally on your desktop
 Please download the script run_analysis.R and set the work directory to the folder in which the
-R file and data folder (unzipped from the file downloaded) is present.
-Also I have named the datafolder as "UCI HAR Dataset", if you have named it different please modify run_analysis.R accordingly before executing.
+data folder "UCI HAR Dataset" (unzipped from the https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) is present.
+The script access the following files in the working directory/UCI HAR Dataset
+activity_labels.txt
+features.txt
+test/
+train/
 
-## To Produce the tidy dataset  
-Run the R script run_analysis.R after modifications if any required. 
+## Output files created under the working directory
+Tidy dataset file DSSmartphonesTidy.csv (comma separated)
+codebook.md
 
-## Output files are
-1. Tidy dataset file DSSmartphonesTidy.txt (tab-delimited text)
-2. codebook.md 
-3. codebook.html
+##Script Flow
+(1) Read all the test and training files
+(2) Combine the files to a data frame in the form of subjects, labels ...
+(3) Read the features from features.txt and filter it to only leave features that are either means ("mean()") or standard deviations ("std()"). 
+(4) A new data frame is then created that includes subjects, labels and the features.
+(5) Read the activity labels from activity_labels.txt and replace the numbers with the text.
+(6) Make a list of columns (includig "subjects" and "label" at the start)
+(7) Tidy-up the list by removing non-alphanumeric character 
+(8) Apply the new list to the data frame
+(9) Create a new data frame by finding the mean for each combination of subject and label using aggregate() function
+(10) Write the tidied data set to a comma separated csv file
+
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,136 +1,52 @@
-#set the UCI HAR Dataset as path to be run from under Github
+#Load package knitr
+library(knitr)
+
+#set the UCI HAR Dataset as path 
 path <- setwd(getwd())
-
-#download the zip file to the current working directory as the file is already downloaded and
-#unzipped commented the below steps 
-
-#url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-#f <- "HARDataset.zip"
-#if (!file.exists(path)) {
-#  dir.create(path)
-#}
-#download.file(url, file.path(path, f))
-
-#Steps to unzip the zip file commenting this as i have uploaded the folder
-
-#executable <- file.path("C:", "Program Files", "7-Zip", "7z.exe")
-#parameters <- "x"
-#cmd <- paste(paste0("\"", executable, "\""), parameters, paste0("\"", file.path(path, f), "\""))
-#system(cmd)
-
 pathIn <- file.path(path, "UCI HAR Dataset")
 
 #Read Subject Files from train and test folders respectively under UCI HAR DATASET folder
-dfSubjectTrain <- fread(file.path(pathIn, "train", "subject_train.txt")) 
-dfSubjectTest <- fread(file.path(pathIn, "test", "subject_test.txt")) 
+dtSubjectTrain <- read.table(file.path(pathIn, "train", "subject_train.txt"), col.names="subject")
+dtSubjectTest  <- read.table(file.path(pathIn, "test", "subject_test.txt"), col.names="subject")
 
-#Read Activity Files from train and test folders respectively under UCI HAR DATASET folder
-dfActivityTrain <- fread(file.path(pathIn, "train", "Y_train.txt")) 
-dfActivityTest <- fread(file.path(pathIn, "test", "Y_test.txt")) 
+#Read label Files from train and test folders respectively under UCI HAR DATASET folder
+dtlabelsTrain <- read.table(file.path(pathIn, "train", "Y_train.txt"), col.names="label")
+dtlabelsTest  <- read.table(file.path(pathIn, "test", "Y_test.txt"), col.names="label") 
 
 #Read Data Files x_train and x_test from train and test folders respectively 
-#under UCI HAR DATASET folder
-fileToDataTable <- function(f) {
-  df <- read.table(f)
-  dt <- data.table(df)
-}
-#convert to data table form
-dtTrain <- fileToDataTable(file.path(pathIn, "train", "X_train.txt"))
-dtTest <- fileToDataTable(file.path(pathIn, "test", "X_test.txt"))
+dtTrainData <- read.table(file.path(pathIn,"train", "X_train.txt"))
+dtTestData  <- read.table(file.path(pathIn,"test", "X_test.txt"))
 
-#Merging the training and the test sets
-#concatenate the tables
-#set meaningful name
-dtSubject <- rbind(dfSubjectTrain, dfSubjectTest)
-setnames(dtSubject, "V1", "subject")
-dtActivity <- rbind(dfActivityTrain, dfActivityTest)
-setnames(dtActivity, "V1", "activityNum")
-dt <- rbind(dtTrain, dtTest)
-
-#Merge columns
-dtSubject <- cbind(dtSubject, dtActivity)
-dt <- cbind(dtSubject, dt)
-
-#Set key
-setkey(dt, subject, activityNum)
+#Merge data sets
+data <- rbind(cbind(dtSubjectTest, dtlabelsTest, dtTestData),
+              cbind(dtSubjectTrain, dtlabelsTrain, dtTrainData))
 
 #Extract only the mean and standard deviation
-dtFeatures <- fread(file.path(pathIn, "features.txt"))
-setnames(dtFeatures, names(dtFeatures), c("featureNum", "featureName"))
+dtFeatures <- read.table(file.path(pathIn, "features.txt"),strip.white=TRUE,stringsAsFactors=FALSE)
+dtFeatures.mean.std <- dtFeatures[grep("mean\\(\\)|std\\(\\)", dtFeatures$V2), ]
+# eliminate subjects and labels in the beginning
+data.mean.std <- data[, c(1, 2, dtFeatures.mean.std$V1+2)]
 
-#Subset only measurements for the mean and standard deviation.
-dtFeatures <- dtFeatures[grepl("mean\\(\\)|std\\(\\)", featureName)]
+# read the labels (activities)
+labels <- read.table(file.path(pathIn,"activity_labels.txt"), stringsAsFactors=FALSE)
+# replace labels in data with label names
+data.mean.std$label <- labels[data.mean.std$label, 2]
 
-#Convert the column numbers to a vector of variable names matching columns in dt
-dtFeatures$featureCode <- dtFeatures[, paste0("V", featureNum)]
-head(dtFeatures)
-dtFeatures$featureCode
+# tidy list by removing non-alphabetic character 
+tidy.colnames <- c("subject", "label",dtFeatures.mean.std$V2)
+tidy.colnames <- gsub("[^[:alpha:]]", "", tidy.colnames)
+# Assign as column names for data
+colnames(data.mean.std) <- tidy.colnames
 
-#Subset these variables using variable names
-select <- c(key(dt), dtFeatures$featureCode)
-dt <- dt[, select, with = FALSE]
+#Compute Mean for each combination of subject and label
+aggr.data <- aggregate(data.mean.std[, 3:ncol(data.mean.std)],
+                       by=list(subject = data.mean.std$subject, 
+                               label = data.mean.std$label),
+                       FUN=mean)
 
-#Use descriptive activity names
-dtActivityNames <- fread(file.path(pathIn, "activity_labels.txt"))
-setnames(dtActivityNames, names(dtActivityNames), c("activityNum", "activityName"))
-
-#Merge activity labels
-dt <- merge(dt, dtActivityNames, by = "activityNum", all.x = TRUE)
-
-#Add activityname as key
-setkey(dt, subject, activityNum, activityName)
-
-#reshape data table
-dt <- data.table(melt(dt, key(dt), variable.name = "featureCode"))
-
-#merge activity name
-dt <- merge(dt, dtFeatures[, list(featureNum, featureCode, featureName)], by = "featureCode", 
-            all.x = TRUE)
-
-#recreate activityname and featurename as a factor class
-dt$activity <- factor(dt$activityName)
-dt$feature <- factor(dt$featureName)
-
-#Separate features and make them redundant
-grepthis <- function(regex) {
-  grepl(regex, dt$feature)
-}
-## Features with 2 categories
-n <- 2
-y <- matrix(seq(1, n), nrow = n)
-x <- matrix(c(grepthis("^t"), grepthis("^f")), ncol = nrow(y))
-dt$featDomain <- factor(x %*% y, labels = c("Time", "Freq"))
-x <- matrix(c(grepthis("Acc"), grepthis("Gyro")), ncol = nrow(y))
-dt$featInstrument <- factor(x %*% y, labels = c("Accelerometer", "Gyroscope"))
-x <- matrix(c(grepthis("BodyAcc"), grepthis("GravityAcc")), ncol = nrow(y))
-dt$featAcceleration <- factor(x %*% y, labels = c(NA, "Body", "Gravity"))
-x <- matrix(c(grepthis("mean()"), grepthis("std()")), ncol = nrow(y))
-dt$featVariable <- factor(x %*% y, labels = c("Mean", "SD"))
-## Features with 1 category
-dt$featJerk <- factor(grepthis("Jerk"), labels = c(NA, "Jerk"))
-dt$featMagnitude <- factor(grepthis("Mag"), labels = c(NA, "Magnitude"))
-## Features with 3 categories
-n <- 3
-y <- matrix(seq(1, n), nrow = n)
-x <- matrix(c(grepthis("-X"), grepthis("-Y"), grepthis("-Z")), ncol = nrow(y))
-dt$featAxis <- factor(x %*% y, labels = c(NA, "X", "Y", "Z"))
-
-#Make sure all possible combinations of features are accountable
-r1 <- nrow(dt[, .N, by = c("feature")])
-r2 <- nrow(dt[, .N, by = c("featDomain", "featAcceleration", "featInstrument", 
-                           "featJerk", "featMagnitude", "featVariable", "featAxis")])
-r1 == r2
-
-#Create Tidy Dataset
-setkey(dt, subject, activity, featDomain, featAcceleration, featInstrument, 
-       featJerk, featMagnitude, featVariable, featAxis)
-dtTidy <- dt[, list(count = .N, average = mean(value)), by = key(dt)]
-
-#f <- file.path(path, "DSSmartphonesTidy.txt")
-#write.table(dtTidy, f, quote = FALSE, sep = "\t", row.names = FALSE)
+# write the data for course upload
+f <- file.path(path, "DSSmartphonesTidy.csv")
+write.table(aggr.data, f, row.names=F, sep = ",",quote=F)
 
 #Make codebook
 knit("makeCodebook.Rmd", output = "codebook.md", encoding = "ISO8859-1", quiet = TRUE)
-
-#Create html format
-markdownToHTML("codebook.md", "codebook.html")
